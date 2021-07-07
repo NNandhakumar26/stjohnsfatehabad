e188183399771a67d65f443e2f67b3935806fcb6
Nandha Kumar <57140186+NNandhakumar26@users.noreply.github.com>
2021-07-07 19:26:20 +0530

    Add files via upload

---
+++ teachers.js
@@ -0,0 +1,43 @@
+(function($) {
+
+	"use strict";
+
+	var fullHeight = function() {
+
+		$('.js-fullheight').css('height', $(window).height());
+		$(window).resize(function(){
+			$('.js-fullheight').css('height', $(window).height());
+		});
+
+	};
+	fullHeight();
+
+	var carousel = function() {
+		$('.teach-carousel').owlCarousel({
+	    loop: true,
+	    autoplay: true,
+	    margin:1,
+	    animateOut: 'fadeOut',
+	    animateIn: 'fadeIn',
+	    nav:true,
+	    dots: false,
+	    autoplayHoverPause: false,
+	    items: 1,
+	    navText : ["<span class='ion-ios-arrow-back'></span>","<span class='ion-ios-arrow-forward'></span>"],
+	    responsive:{
+	      0:{
+	        items:1
+	      },
+	      600:{
+	        items:3
+	      },
+	      1000:{
+	        items:4
+	      }
+	    }
+		});
+
+	};
+	carousel();
+
+})(jQuery);
